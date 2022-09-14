1b106db83cbb83caa90dd8023eba58f0f02d4089
Umesh Shrestha <stha.ums@gmail.com>
2022-09-14 08:56:36 +0545

    use with params

--- lib/blog/presentation/screens/widgets/blog_list.dart
+++ lib/blog/presentation/screens/widgets/blog_list.dart
@@ -7,45 +7,46 @@ import 'package:go_router_learn/config/routes/routes.dart';
 class BlogLists extends StatelessWidget {
   final BlogController blogController;
 
-  const BlogLists({
-    super.key,
-    required this.blogController,
-  });
+  const BlogLists({super.key, required this.blogController});
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Blog>>(
-        stream: blogController.stream,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          final allBlogs = snapshot.data!;
+      stream: blogController.stream,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        final allBlogs = snapshot.data!;
 
-          return ListView.separated(
-            itemCount: allBlogs.length,
-            separatorBuilder: (_, __) => const Divider(),
-            itemBuilder: ((context, index) {
-              return ListTile(
-                title: Text(allBlogs[index].title),
-                subtitle: Text(
-                  allBlogs[index].content,
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: IconButton(
-                  onPressed: () {
-                    blogController.toggleFavorite(allBlogs[index].id);
-                  },
-                  icon: Icon(allBlogs[index].isFavorite ? Icons.favorite : Icons.favorite_border_outlined),
-                ),
-                onTap: () {
-                  GoRouter.of(context).go(AppRouter.singleArticle, extra: allBlogs[index]);
-                  GoRouter.of(context).go(AppRouter.singleArticleWithParams(allBlogs[index].id));
+        return ListView.separated(
+          itemCount: allBlogs.length,
+          separatorBuilder: (_, __) => const Divider(),
+          itemBuilder: ((context, index) {
+            return ListTile(
+              title: Text(allBlogs[index].title),
+              subtitle: Text(
+                allBlogs[index].content,
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+              ),
+              trailing: IconButton(
+                onPressed: () {
+                  blogController.toggleFavorite(allBlogs[index].id);
                 },
-              );
-            }),
-          );
-        });
+                icon: Icon(allBlogs[index].isFavorite ? Icons.favorite : Icons.favorite_border_outlined),
+              ),
+              onTap: () {
+                // route with arguments
+                // GoRouter.of(context).go(AppRouter.singleArticle, extra: allBlogs[index]);
+
+                // route with parameter
+                GoRouter.of(context).go(AppRouter.singleArticleWithParams(allBlogs[index].id));
+              },
+            );
+          }),
+        );
+      },
+    );
   }
 }
